20ac21724c3f28e563cc6b29747c896b98220a4b
motioncomposer <srilekha@motioncomposer.com>
2024-03-03 18:31:11 +0100

    Exercise 1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -4,8 +4,6 @@ public class Exercise1 {
 
     public static void main(String[] args) {
 
-        // Use StreamSources.intNumbersStream() and StreamSources.userStream()
-
         // Print all numbers in the intNumbersStream stream
         StreamSources.intNumbersStream().forEach(System.out::println);
 
